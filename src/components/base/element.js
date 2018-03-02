84ae201ea6e0a1fc39bdfe3e3f2f58c3cdabaaa8
xianhaiyuan <xianhaiyuan@gmail.com>
2018-03-02 13:34:44 +0800

    .

--- src/components/base/element.js
+++ src/components/base/element.js
@@ -1,17 +1,19 @@
-import Vue from 'vue'
-import 'element-ui/lib/theme-chalk/index.css'
+import Vue from 'vue';
+// import 'element-ui/lib/theme-chalk/index.css'
 import {
   Button,
-  Select,
   Input,
   Form,
   FormItem
-} from 'element-ui'
+} from 'element-ui';
 Vue.prototype.$ELEMENT = {
   size: 'small'
 }
-Vue.component(Button.name, Button)
-Vue.component(Select.name, Select)
-Vue.component(Input.name, Input)
-Vue.component(Form.name, Form)
-Vue.component(FormItem.name, FormItem)
+// Vue.component(Button.name, Button)
+// Vue.component(Input.name, Input)
+// Vue.component(Form.name, Form)
+// Vue.component(FormItem.name, FormItem)
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Form)
+Vue.use(FormItem)
