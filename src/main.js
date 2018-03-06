a5915fe642bd4438fff58e6e86bd1ba267a5bd55
xianhaiyuan <xianhaiyuan@gmail.com>
2018-03-06 21:15:20 +0800

    .

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import axios from 'axios'
 import store from './vuex/store'
 import './utils/element'
 import sessionManager from './utils/sessionManager'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import VueQuillEditor from 'vue-quill-editor'
+
+Vue.use(VueQuillEditor)
 
 Vue.prototype.$session = sessionManager
 Vue.prototype.$ajax = axios
--- src/pages/publish.vue
+++ src/pages/publish.vue
@@ -1,13 +1,76 @@
 <template>
-  <div>
-    {{$route.params.id}}
+  <div class="publish">
+    <el-input autofocus v-model="tit" placeholder="请输入帖子主题"></el-input>
+    <quill-editor ref="myTextEditor" v-model="content" :config="editorOption" @blur="onEditorBlur($event)" @focus="onEditorFocus($event)" @ready="onEditorReady($event)">
+    </quill-editor>
+    <div>
+      <el-button class="u-submit" type="primary" @click="showContent">提交
+        <i class="el-icon-upload el-icon--right"></i>
+      </el-button>
+      <!-- <div v-html="this.content"></div> -->
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { quillEditor } from "vue-quill-editor";
+export default {
+  data() {
+    return {
+      content: "<h2>I am Example</h2>",
+      editorOption: {
+        // something config
+      },
+      tit: ""
+    };
+  },
+  methods: {
+    onEditorBlur(editor) {
+      // console.log("editor blur!", editor.container.innerHTML);
+      console.log("editor blur!", editor.getContents());
+    },
+    onEditorFocus(editor) {
+      console.log("editor focus!", editor);
+    },
+    onEditorReady(editor) {
+      console.log("editor ready!", editor);
+    },
+    onEditorChange({ editor, html, text }) {
+      // console.log('editor change!', editor, html, text)
+      this.content = html;
+      console.log(html);
+    },
+    showContent() {
+      console.log(this.content);
+    }
+  },
+  computed: {
+    editor() {
+      return this.$refs.myTextEditor.quillEditor;
+    }
+  },
+  components: {
+    quillEditor
+  }
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.publish {
+  padding: 20px;
+}
+.ql-container {
+  height: 350px;
+}
+.u-submit {
+  float: right;
+  margin-top: 20px;
+}
+.el-input {
+  width: 48%;
+  margin-bottom: 16px;
+}
+.ql-container.ql-snow {
+  height: 350px;
+}
 </style>
